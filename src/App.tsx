c3b9e8ac0bb21570cf50cce9f79f841dc40ad1e5
donastien <donastien.karoum@gmail.com>
2020-06-01 16:31:53 +0200

    Affichage Grille

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,26 @@ function App() {
     return rows;
   });
 
-  console.log(grid);
-  
   return (
-    <div>
-      yo
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+            key={`${i}-${k}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i][k] ? 'pink' : undefined,
+              border: 'solid 1px black',
+            }}
+          />
+        ))
+      )}
     </div>
   );
 }
