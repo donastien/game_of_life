33bb46b400e0ad59060e539b7fb2faa251db8eb2
donastien <donastien.karoum@gmail.com>
2020-06-01 16:03:11 +0200

    Create Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const numRows = 50;
+const numCols = 50;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
+  console.log(grid);
+  
   return (
     <div>
-      
+      yo
     </div>
   );
 }
